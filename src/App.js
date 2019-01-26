82793d26c8a491e53705eaf935e7770c4e7c3897
Venom <saurabhnarhe123@gmail.com>
2019-01-26 11:02:32 +0530

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-            Learn React
+          rtCamp - FB Challenge
         </header>
       </div>
     );
